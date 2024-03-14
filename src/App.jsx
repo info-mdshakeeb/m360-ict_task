b44ff0c511ba9d01084bc8a3839ed9cec1ec834c
itzshakeeb <itzshakeeb@gmail.com>
2024-03-14 17:45:54 +0600

    todoSlice added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import { useSelector } from "react-redux"
-import { useTodo } from "./redux/features/todo/todoSlice"
-
 function App() {
-  const todo = useSelector(useTodo)
-  console.log(todo);
+
   return (
-    <>
-    </>
-  )
+    <></>
+  );
 }
 
-export default App
+export default App;
--- src/redux/features/todo/todoSlice.js
+++ src/redux/features/todo/todoSlice.js
@@ -1,16 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 
-const initialState = {
-  test: "hello"
-}
+const initialState = JSON.parse(localStorage.getItem('tasks')) || []
 
 export const todoSlice = createSlice({
   name: "todo",
   initialState,
-  reducers: {}
+  reducers: {
+    addTask: (state, { payload }) => { state.push(payload) },
+    editTask: (state, { payload }) => state.map(task => task.id === payload.id ? {
+      ...task, text: payload.text
+    } : task),
+    toggleStatus: (state, { payload }) => state.map(task => task.id === payload.id ? {
+      ...task, status: !task.status
+    } : task),
+    delateTask: (state, { payload }) => state.filter(task => task.id === payload.id),
+    delateAll: () => [],
+  }
 })
-
 export default todoSlice.reducer
 
 // todo:  for selector :
