7da5ea5582746f176a8b7735bb2be936e7e9e4c3
itzshakeeb <itzshakeeb@gmail.com>
2024-03-15 19:48:39 +0600

    finish Requirements

--- src/components/action-task.jsx
+++ src/components/action-task.jsx
@@ -14,20 +14,23 @@ const ActionTasks = ({ filter }) => {
         .filter((task) => filter === 'all' || task.priority === filter)
         .map((task) => (
           <div key={task.id} className={`border p-2 mt-2 ${task.completed ? (task.priority === 'low' ? 'border-green-500' : task.priority === 'medium' ? 'border-yellow-500' : 'border-red-500') : ''}`}>
+
             {edit?.id === task.id ?
               <UpdateTask edit={edit} setEdit={setEdit} task={task} />
-              : <p className={`${task.completed ? 'line-through' : ''}`}>{task.name}</p>
-            }
+              : <p className={`${task.completed ? 'line-through' : ''}`}> {task.name} </p>}
+
             <button
               onClick={() => dispatch(toggleStatus(task))}
               className="p-2 mt-4 mr-2 text-white bg-green-500 rounded">
               {task.completed ? 'Mark as incomplete' : 'Mark as complete'}
             </button>
+
             {!edit?.id && <button
               onClick={() => setEdit(task)}
               className="p-2 mt-4 mr-2 text-white bg-yellow-500 rounded">
               Edit
             </button>}
+
             <button onClick={() => dispatch(delateTask(task))}
               className="p-2 mt-4 text-white bg-red-500 rounded">
               Delete
